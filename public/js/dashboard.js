2e0827a1e8c593446ef805704a5aa24eb70f62cc
aditasyhari <adit.asyhari16@gmail.com>
2022-11-16 00:22:31 +0700

    rev

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
             if($(this).is(':checked')) {
                 harga += $(this).data('harga');
                 if($(this).data('paket') != 'a') {
-                    ongkir = 90000;
-                    $('#ongkir').html('Rp 90.000');
+                    ongkir = 100000;
+                    $('#ongkir').html('Rp 100.000');
                 }
 
                 data_arr.push({
